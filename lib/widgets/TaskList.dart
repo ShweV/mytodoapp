4db54195363043e61bbad7bcc42dcfa370da179d
shwvasud1 <shwetha.vasudev@publicissapient.com>
2021-06-10 00:42:14 +0530

    Task list changes added

--- lib/widgets/TaskList.dart
+++ lib/widgets/TaskList.dart
@@ -16,34 +16,38 @@ class TaskList extends StatefulWidget {
 
 class TaskListState extends State<TaskList> {
   static String item;
-  final items = List<String>.generate(20, (i) => 'Item ${i + 1}');
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body : ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            final item = items[index];
-            return Dismissible(
-              // Each Dismissible must contain a Key. Keys allow Flutter to
-              // uniquely identify widgets.
-              key: Key(item),
-              // Provide a function that tells the app
-              // what to do after an item has been swiped away.
-              onDismissed: (direction) {
-                // Remove the item from the data source.
-                setState(() {
-                  items.removeAt(index);
-                });
-              },
-              // Show a red background as the item is swiped away.
-              background: Container(color: Colors.black87),
-              child: ListTile(
-                title: Text('$item'), tileColor: Colors.amber,),
-            );
-          },
+        body : FutureBuilder<List<Todo>> (
+            future: DatabaseHelper().retrieveTodos(),
+            builder: (context, snapshot) {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    final item = snapshot.data[index];
+                    return Dismissible(
+                      // Each Dismissible must contain a Key. Keys allow Flutter to
+                      // uniquely identify widgets.
+                      key: Key(item.title),
+                      // Provide a function that tells the app
+                      // what to do after an item has been swiped away.
+                      onDismissed: (direction) {
+                        // Remove the item from the data source.
+                        setState(() {
+                          snapshot.data.removeAt(index);
+                        });
+                      },
+                      // Show a red background as the item is swiped away.
+                      background: Container(color: Colors.black87),
+                      child: ListTile(
+                        title: Text('$item.title'), tileColor: Colors.amber,),
+                    );
+                  },
+                );
+            }
         ),
       ),
     );
